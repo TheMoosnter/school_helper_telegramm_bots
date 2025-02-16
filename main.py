c8e703b5cd5c1c756993b37f527ec927f97d9520
TheMoon <fomenkovany2009@gmail.com>
2025-02-16 21:08:13 +0400

    aiogram usage

--- main.py
+++ main.py
@@ -1,71 +1,78 @@
 import datetime
 from threading import Thread
+import asyncio
 
 from loguru import logger
-from telebot import TeleBot
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
 
 from config import CHAT_ID, STUDENT_LIST, TOKEN, DataManager
 from utils import DutyBot
 
 
-def main():
+logger.info("Launching a telegram bot")
+now_one = datetime.datetime.now()
+logger.info(f"Hours: {now_one.hour}, minutes: {now_one.minute}")
+
+bot = Bot(token=str(TOKEN))
+dp = Dispatcher()
+data_manager = DataManager()
+duty_bot = DutyBot(bot, CHAT_ID, STUDENT_LIST, data_manager)
+
+# Command handlers
+@dp.message(Command("start_day"))
+async def _start_day(message: Message):
+    await duty_bot.new_day()
+
+@dp.message(Command("end_day"))
+async def _end_day(message: Message):
+    await duty_bot.end_day()
+
+@dp.message(Command("skip"))
+async def skip(message: Message):
     """
-    Entry point to the application. Initializes the bot and starts the main processes.
+    Adds the current duty officer to the absent list and moves on to the next one.
     """
-    logger.info("Launching a telegram bot")
-    now_one = datetime.datetime.now()
-    logger.info(f"Hours: {now_one.hour}, minutes: {now_one.minute}")
-    bot = TeleBot(TOKEN)
-    data_manager = DataManager()
-    duty_bot = DutyBot(bot, CHAT_ID, STUDENT_LIST, data_manager)
+    await duty_bot.process_skip()
 
-    # Command handlers
-    @bot.message_handler(commands=["start_day"])
-    def _start_day(message):
-        duty_bot.new_day()
+@dp.message(Command("put"))
+async def put(message: Message):
+    """
+    Place the missing student on duty.
+    Command format: /put <Student_name>.
+    """
+    student_name = message.text.replace("/put ", "").strip()
+    await duty_bot.process_put(student_name, message.chat.id)
 
-    @bot.message_handler(commands=["end_day"])
-    def _end_day(message):
-        duty_bot.end_day()
+@dp.message(Command("set"))
+async def set_duty(message: Message):
+    """
+    Set a specific student as the on-duty student.
+    Command format: /set <Student_name>.
+    """
+    student_name = message.text.replace("/set ", "").strip()
+    await duty_bot.process_set(student_name, message.chat.id)
 
-    @bot.message_handler(commands=["skip"])
-    def skip(message):
-        """
-        Adds the current duty officer to the absent list and moves on to the next one.
-        """
-        duty_bot.process_skip()
+@dp.message(Command("skip_queue"))
+async def skip_without_queue(message: Message):
+    """
+    Skip the current duty without taking into account his absence.
+    """
+    await duty_bot.skip_queue()
 
-    @bot.message_handler(commands=["put"])
-    def put(message):
-        """
-        Place the missing student on duty.
-        Command format: /put <Student_name>.
-        """
-        student_name = message.text.replace("/put ", "").strip()
-        duty_bot.process_put(student_name, message.chat.id)
+# Start a background thread to check the time
+# Thread(target=duty_bot.check_time).start()
 
-    @bot.message_handler(commands=["set"])
-    def set_duty(message):
-        """
-        Set a specific student as the on-duty student.
-        Command format: /set <Student_name>.
-        """
-        student_name = message.text.replace("/set ", "").strip()
-        duty_bot.process_set(student_name, message.chat.id)
 
-    @bot.message_handler(commands=["skip_queue"])
-    def skip_without_queue(message):
-        """
-        Skip the current duty without taking into account his absence.
-        """
-        duty_bot.skip_queue()
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN)
 
-    # Start a background thread to check the time
-    Thread(target=duty_bot.check_time).start()
-
-    # Bot start
-    bot.polling()
+    # And the run events dispatching
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
+
--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ class DutyBot:
         self.data_manager = data_manager
         self.mes = None
 
-    def check_student_in_list(self, student, list):
+    async def check_student_in_list(self, student, list):
         """
         Checks if a student exists in the given list.
 
@@ -28,7 +28,7 @@ class DutyBot:
         """
         return student in list
 
-    def new_day(self, student_index=None):
+    async def new_day(self, student_index=None):
         """
         Updates information about the person on duty for the new day.
         """
@@ -39,28 +39,28 @@ class DutyBot:
             else self.data_manager.get("id", 0)
         )
         abs_stud = ", ".join(self.data_manager.get("absent_students", []))
-        self.mes = self.bot.send_message(
+        self.mes = await self.bot.send_message(
             self.chat_id,
             f"{self.student_list[student_index]} is on duty today\nMissed duty: {abs_stud}",
         )
         logger.info(f"New duty selected: {self.student_list[student_index]}")
 
-    def end_day(self):
+    async def end_day(self):
         """
         Ends the current duty day and updates the counter.
         """
         if self.data_manager.get("a") == 0:
-            self.increment_id()
-            self.check_id()
+            await self.increment_id()
+            await self.check_id()
         self.data_manager.set("a", 0)
 
-    def increment_id(self):
+    async def increment_id(self):
         """
         Increments the ID of the current duty officer.
         """
         self.data_manager.set("id", self.data_manager.get("id", 0) + 1)
 
-    def check_id(self):
+    async def check_id(self):
         """
         Checks if the identifier is greater than the number of students.
         """
@@ -68,23 +68,23 @@ class DutyBot:
             self.data_manager.set("id", 1)
             logger.info("Student list was started anew.")
 
-    def process_skip(self):
+    async def process_skip(self):
         """
         Adds the current duty officer to the absent list and moves on to the next one.
         """
         if self.mes:
-            self.bot.delete_message(self.chat_id, self.mes.id)
+            await self.bot.delete_message(self.chat_id, self.mes.message_id)
         logger.info(
             f"Student {self.student_list[self.data_manager.get("id", 0)]} was marked as absent."
         )
         self.data_manager.append_to_list(
             "absent_students", self.student_list[self.data_manager.get("id", 0)]
         )
-        self.increment_id()
-        self.check_id()
-        self.new_day()
+        await self.increment_id()
+        await self.check_id()
+        await self.new_day()
 
-    def process_put(self, student_name, personal_mes_chat_id):
+    async def process_put(self, student_name, personal_mes_chat_id):
         """
         Sets the missing student as the duty.
 
@@ -92,47 +92,47 @@ class DutyBot:
         :param student_name: Author chat id (for send error message)
         """
         absent_students = self.data_manager.get("absent_students", [])
-        if self.check_student_in_list(student_name, absent_students):
+        if await self.check_student_in_list(student_name, absent_students):
             if self.mes:
-                self.bot.delete_message(self.chat_id, self.mes.id)
+                await self.bot.delete_message(self.chat_id, self.mes.message_id)
             logger.info("The absent student was appointed on duty.")
             self.data_manager.set("a", self.student_list.index(student_name))
             self.data_manager.remove_from_list("absent_students", student_name)
-            self.new_day(self.data_manager.get("a"))
+            await self.new_day(self.data_manager.get("a"))
         else:
-            self.bot.send_message(
+            await self.bot.send_message(
                 personal_mes_chat_id, "No such person found in the list"
             )
 
-    def process_set(self, student_name, personal_mes_chat_id):
+    async def process_set(self, student_name, personal_mes_chat_id):
         """
         Sets the specified student as the on-duty student.
 
         :param student_name: Student name.
         """
-        if self.check_student_in_list(student_name, self.student_list):
+        if await self.check_student_in_list(student_name, self.student_list):
             if self.mes:
-                self.bot.delete_message(self.chat_id, self.mes.id)
+                await self.bot.delete_message(self.chat_id, self.mes.message_id)
             logger.info("The student was appointed on duty manually.")
             self.data_manager.set("id", self.student_list.index(student_name))
-            self.new_day(self.data_manager.get("id"))
+            await self.new_day(self.data_manager.get("id"))
         else:
-            self.bot.send_message(
+            await self.bot.send_message(
                 personal_mes_chat_id, "No such person found in the list"
             )
 
-    def skip_queue(self):
+    async def skip_queue(self):
         """
         Allows the current person on duty to pass without taking into account their absence.
         """
         if self.mes:
-            self.bot.delete_message(self.chat_id, self.mes.id)
+            await self.bot.delete_message(self.chat_id, self.mes.message_id)
         logger.info("The student skipped his duty.")
-        self.increment_id()
-        self.check_id()
-        self.new_day()
+        await self.increment_id()
+        await self.check_id()
+        await self.new_day()
 
-    def check_time(self):
+    async def check_time(self):
         """
         Background task for checking time. Updates the duty officer every day at 8:00.
         """
@@ -140,8 +140,8 @@ class DutyBot:
             now = datetime.datetime.now()
             if now.weekday() < 6 and now.hour == 23 and now.minute == 10:
                 logger.info("Day duty update")
-                self.end_day()
-                self.new_day()
+                await self.end_day()
+                await self.new_day()
                 time.sleep(60)
             else:
                 time.sleep(59)
